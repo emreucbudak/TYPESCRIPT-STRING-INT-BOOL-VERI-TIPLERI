cfd79cc7e383dc18d70cb0f742afa73db93d974a
Emre Üçbudak <147994857+emreucbudak@users.noreply.github.com>
2024-09-05 15:17:54 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+var message = "Hello World";
+var age = 36;
+var isLoad = false;
+console.log(typeof message);
+console.log(typeof age);
+console.log(typeof isLoad);
---
+++ app.ts
@@ -0,0 +1,6 @@
+let message:string = "Hello World"
+let age:number = 36
+let isLoad:boolean = false
+console.log(typeof message)
+console.log(typeof age)
+console.log(typeof isLoad)
